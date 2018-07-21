b270d65a424661cc9cb23902e7cd73f4c0b46ab7
Marcin Jaworski <m.a.jaworski96@gmail.com>
2018-07-21 15:37:46 +0200

    Dialog fix

--- TimeMeasure/TimeMeasure/TimeMeasure/View/Dialog.cs
+++ TimeMeasure/TimeMeasure/TimeMeasure/View/Dialog.cs
@@ -18,7 +18,7 @@ namespace TimeMeasure.View
 
         public async Task<bool> YesNoDialog(string title, string message)
         {
-            return await page.DisplayAlert("Clear data", "Do you want remove this period?", "Yes", "No");
+            return await page.DisplayAlert(title, message, "Yes", "No");
         }
     }
 }
